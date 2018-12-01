fc35fd221cc1f81adb44bb290372eb7d36ea6f60
knh369 <knh369@konyadh.gov.tr>
2018-12-01 15:08:44 +0300

    weekend edu

--- routes/cards.js
+++ routes/cards.js
@@ -9,11 +9,18 @@ const { cards } = data; // Şununla aynı anlama geliyor ==> const cards = data.
 
 // '/:id' yapısında, ":" sonrasındaki isim değişken olarak kullanılır.
 router.get('/:id', (req, res) => { // kendi route u içerisinde taşıdıktan sonra "/cards" yerine "/" kullandık. Çünkü app.js içerisinde app.use('/cards') yolunu kullanıyoruz
-    res.locals.soru = cards[req.params.id].question; // değişkenler için tercih ettiğim tanımlama yapısı (res.locals)
-    res.locals.ipucu = cards[req.params.id].hint; // req.params.id yapısı ile, url nin sonundaki sayıyı yakalayabildik
+    const { id } = req.params; // "/cards/2?kartYüzü=soru" kalıbında "2" kısmını yakalar
+    const { kartYüzü } = req.query; // "/cards/2?kartYüzü=soru" kalıbında "soru" kısmını yakalar
+    const sorucevapMetni = cards[id][kartYüzü]; // örneğimizde: cards[2][soru] dan ikinci soruyu getirir
+    const { ipucu } = cards[id]; // örneğimizde cards[2][ipucu] yolu ile ikinci ipucunu getirir
+    
+    const ceptekiVeri = { sorucevapMetni, ipucu }; // örneğimizde { " "cards[2][soru]", "cards[2][ipucu]" "} olarak 2.sıradaki soruyu ve ipucunu depolar
+    //res.locals.soru = cards[req.params.id].soru; // değişkenler için tercih ettiğim tanımlama yapısı (res.locals)
+    //res.locals.ipucu = cards[req.params.id].ipucu; // req.params.id yapısı ile, url nin sonundaki sayıyı yakalayabildik
     // LOOP ÖRNEĞİ (01)
     //res.locals.renkler = colors;
-    res.render('card');
+    res.render('card', ceptekiVeri);
 });
 
 module.exports = router;
+
