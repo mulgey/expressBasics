4ee7984a6ae20fbf61abb09babe816201a04a9c7
knh369 <knh369@konyadh.gov.tr>
2018-12-04 16:20:49 +0300

    daily edu

--- routes/cards.js
+++ routes/cards.js
@@ -8,13 +8,20 @@ const { data } = require('../data/cardsData.json');
 const { cards } = data; // Şununla aynı anlama geliyor ==> const cards = data.cards;
 
 // '/:id' yapısında, ":" sonrasındaki isim değişken olarak kullanılır.
+router.get('/', (req, res) => {
+    const kartSayısı = cards.length;
+    const kartIDsi = Math.floor( Math.random() * kartSayısı); // standart olan rastgele formülü içerisinde kartsayısı bilgisini kullanarak oluşturduğumuz değeri kaydedip, adres çubuğuna pasladık.
+    res.redirect( `/cards/${kartIDsi}?kartYüzü=soru` ); 
+});
+
 router.get('/:id', (req, res) => { // kendi route u içerisinde taşıdıktan sonra "/cards" yerine "/" kullandık. Çünkü app.js içerisinde app.use('/cards') yolunu kullanıyoruz
     const { id } = req.params; // "/cards/2?kartYüzü=soru" kalıbında "2" kısmını yakalar
     const { kartYüzü } = req.query; // "/cards/2?kartYüzü=soru" kalıbında "soru" kısmını yakalar
     const sorucevapMetni = cards[id][kartYüzü]; // örneğimizde: cards[2][soru] dan ikinci soruyu getirir
     const { ipucu } = cards[id]; // örneğimizde cards[2][ipucu] yolu ile ikinci ipucunu getirir
-    
-    const ceptekiVeri = { id, sorucevapMetni }; // { "2", "cards[2][soru]" } verilerini, res.locals a itelemek üzere depolar
+    const isim = req.cookies.kurabiye; // card.pug dan extends --> layout.pug içerisindeki isim anahtarına kurabiyedeki değeri verdik
+
+    const ceptekiVeri = { id, sorucevapMetni, isim }; // { "2", "cards[2][soru]", "isim" } verilerini, res.locals a itelemek üzere depolar
     if (kartYüzü === 'soru') {
         ceptekiVeri.ipucu = ipucu; // örneğimizde ceptekiVeri ye cards[2][ipucu] nu ekliyoruz ama bu kısımda aydınlamaya ihtiyacım var
         ceptekiVeri.gösterilecek = 'cevap'; // ceptekiVeri ye res.locals verilerini eklemeye devam ediyoruz aşağı doğru
@@ -22,6 +29,8 @@ router.get('/:id', (req, res) => { // kendi route u içerisinde taşıdıktan so
     } else if (kartYüzü === 'cevap') {
         ceptekiVeri.gösterilecek = 'soru';
         ceptekiVeri.gösterilecekTaraf = 'Soru';
+    } else {
+        res.redirect( `/cards/${id}?kartYüzü=soru` );
     }
     
     //res.locals.soru = cards[req.params.id].soru; // değişkenler için tercih ettiğim tanımlama yapısı (res.locals)
